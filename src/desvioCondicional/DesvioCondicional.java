29513b4eaa7a6dd71410481b722d4c6893c214fa
RafaelaCorreaFernandes <rafaela63466666@alunos.sc.senac.br>
2026-04-02 16:25:47 -0300

    exercicio 3

--- src/desvioCondicional/DesvioCondicional.java
+++ src/desvioCondicional/DesvioCondicional.java
@@ -1,12 +1,23 @@
 package desvioCondicional;
+
 import java.util.Scanner;
 
 public class DesvioCondicional {
 	public static void main(String[] args) {
 		
 		//exercicio1();	
-		 exercicio2();
-	     }
+		//exercicio2();
+		exercicio3();
+	
+	}
+
+		
+	
+
+	
+
+
+
 
 	static void exercicio1() {
 		Scanner entradaUser = new Scanner (System.in);
@@ -39,6 +50,28 @@ public class DesvioCondicional {
 		if (numero1 < numero2) {
 			System.out.println(numero1 + " é menor que " + numero2);
 		}
+		
+		static void exercicio3() {
+		 Scanner sc = new Scanner (System.in);
+		 
+		 System.out.println("Informe um numero ");
+		 int numero = sc.nextInt();
+		 if (numero > 0) {
+			 System.out.println("Esse número é positivo ");
+			 }
+		 if (numero < 0) {
+			 System.out.println("Esse número é negativo ");
+		 }
+		 if (numero == 0) {
+			 System.out.println("Esse número é igual a 0 ");
+			 
+	
+			 }
+		
+		 
+		 
+		
+		
 	}
 	
 	
